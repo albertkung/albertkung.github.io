d7fb2bf33ae11fba982a23ecf7fd614f48328dc4
Albert Kung <albertkung43@gmail.com>
2016-10-13 15:03:13 -0400

    consolidate js

--- js/console.js
+++ js/console.js
@@ -1,3 +1,34 @@
+// Intro commands
+var my_text = "^^^^^^Hello world! ^^^^^^My name is Albert.^^ Welcome to my website :)";
+var max_length = my_text.length;
+
+function animeme() {
+    var pos = 0;
+    var id = setInterval(frame, 50);
+    function frame() {
+        if (pos == max_length) {
+            clearInterval(id);
+        }
+        else if (my_text.charAt(pos) == '^') {
+            clearInterval(id);
+            sleep(50).then(() => {
+                pos++;
+                id = setInterval(frame, 50);
+            })
+        }
+        else {
+            $("#console-output").append(my_text.charAt(pos));
+            pos++;
+        }
+    }
+}
+
+function sleep(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+animeme();
+
 // Help/execute modals
 var helpModal = document.getElementById('help-modal');
 $("#help").click(function() {
